8c197017e93fa1c56e930864aefe47b596c7ce25
Bam Khatri <bamkhatri@Bams-MacBook-Pro.local>
2024-08-01 13:01:13 +1000

    mutliply  added

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         </div>
         <h2>Result: {result}</h2>
         <h5>Here is yours Calculation</h5>
-        {/* subtraction added */}
+        {/* subtraction addeds */}
       </header>
     </div>
   );
