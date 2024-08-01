61082f1eb0fc5cea35138eda8e8ea9793e33f4a6
Bam Khatri <bamkhatri@Bams-MacBook-Pro.local>
2024-08-01 12:59:23 +1000

    dividsion added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [result, setResult] = useState(0);
-
+  //addition is done here
   const handleAdd = () => {
     setResult(Number(num1) + Number(num2));
   };
+  // subtraction is done here
   const handleSubtract = () => {
     setResult(Number(num1) - Number(num2));
   };
-
+  //multiply is done here
+  const handleMultiply = () => {
+    setResult(Number(num1) * Number(num2));
+  };
+  //division is done here
+  const handleDivide = () => {
+    setResult(Number(num1) / Number(num2));
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -32,6 +40,8 @@ function App() {
           />
           <button onClick={handleAdd}>Add</button>
           <button onClick={handleSubtract}>Subtract</button>
+          <button onClick={handleMultiply}>Multiply</button>
+          <button onClick={handleDivide}>Divide</button>
         </div>
         <h2>Result: {result}</h2>
         <h5>Here is yours Calculation</h5>
