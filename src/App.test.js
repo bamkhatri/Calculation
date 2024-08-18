3858c2389e69b0849d0f8d7700f09cbbb9c2de29
Bam Khatri <bamkhatri24@gmai.com>
2024-08-18 12:05:26 +1000

    added some incorrect testing code to check in jenkins console

--- src/App.test.js
+++ src/App.test.js
@@ -50,8 +50,19 @@ describe("App Component", () => {
     fireEvent.click(screen.getByText("Multiply"));
     expect(screen.getByText(/Result:/)).toHaveTextContent("Result: 50");
   });
+  // This multiple numbers incorrectly and should not pass
+  test("multiplies numbers incorrectly", () => {
+    fireEvent.change(screen.getByPlaceholderText("Enter first number"), {
+      target: { value: "10" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Enter second number"), {
+      target: { value: "5" },
+    });
+    fireEvent.click(screen.getByText("Multiply"));
+    expect(screen.getByText(/Result:/)).toHaveTextContent("Result: 60");
+  });
 
-  // This test shoud pass now
+  // This test shoud fail now
   test("divides numbers incorrectly (intentionally failing)", () => {
     fireEvent.change(screen.getByPlaceholderText("Enter first number"), {
       target: { value: "10" },
@@ -60,6 +71,6 @@ describe("App Component", () => {
       target: { value: "5" },
     });
     fireEvent.click(screen.getByText("Divide"));
-    expect(screen.getByText(/Result:/)).toHaveTextContent("Result: 2");
+    expect(screen.getByText(/Result:/)).toHaveTextContent("Result: 3");
   });
 });
